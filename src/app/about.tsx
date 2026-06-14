175ccfb23ec8108ffe2acce820dd9bee3abe1190
Joaquín Antonino <joaqantonino@gmail.com>
2026-06-13 21:22:18 -0300

    add scroll to top on about screen

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,10 +1,20 @@
 import { ScrollView } from "react-native";
 import ContactSection from "../components/ContactSection";
 import UbicationSection from "../components/UbicationSection";
+import { useRef, useCallback } from "react";
+import { useFocusEffect } from "expo-router";
 
 export default function AboutScreen() {
+   const scrollRef = useRef<ScrollView>(null);
+
+  useFocusEffect(
+    useCallback(() => {
+      scrollRef.current?.scrollTo({ y: 0, animated: false });
+    }, [])
+  );
+
   return (
-    <ScrollView style={{flex: 1, backgroundColor: "#030712"}}>
+    <ScrollView ref={scrollRef} style={{flex: 1, backgroundColor: "#030712"}}>
       <UbicationSection />
       <ContactSection />
     </ScrollView>
